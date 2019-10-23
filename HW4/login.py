dab29adf9260335eb8a4280c1caf5be958cfd756
good-engineer <ms_danesh@hotmail.com>
2019-10-23 12:51:53 +0900

    finished login part

--- HW4/login.py
+++ HW4/login.py
@@ -32,4 +32,22 @@ def auth(ID, name):
     user_connect = get_connect()
 
     #check if student exists
-
+    
+    c = user_connect.cursor()
+    c.execute("SELECT FROM student\
+            WHERE ID=\"%s\" and name=\"%s\""%(ID,name))
+    data = c.fetchone()
+    if data is not None
+       user_acc.set_attrs(ID,name,0,user_connect)
+       return
+   else
+       c.execute("SELECT FROM instructor\
+               WHERE ID=\"%s\" and name=\"%s\""%(ID,name))
+       data = c.fetchone()
+       if data is not None
+           user_acc.set_attrs(ID,name,1,user_connect)
+           return
+       else
+           print("Your ID and name is not valid!")
+           return_connect(user_connect)
+           return
